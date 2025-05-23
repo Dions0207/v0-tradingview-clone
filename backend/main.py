7fe1122190abf878ba4b6dc3a8a0c02e862d5243
Dions0207 <driversdions@gmail.com>
2025-05-23 13:08:32 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.routes import auth, trading, data
+from routes import auth, trading, data
 import uvicorn
 
 app = FastAPI(title="TradingView IQ Option API")
